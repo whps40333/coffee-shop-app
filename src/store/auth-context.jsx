e5af4129b5f8fd041da9f296bed9b8803a9daa68
whps40333 <whps40333@gmail.com>
2023-06-10 22:46:34 +0800

    change router name

--- src/store/auth-context.jsx
+++ src/store/auth-context.jsx
@@ -71,7 +71,7 @@ export const AuthContextProvider = (props) => {
       const token = generateToken();
       localStorage.setItem("token", token);
       setIsLoggedIn(true);
-      navigate("main");
+      navigate("/coffee-shop-app/main");
     } catch (error) {
       console.log(error);
     }
